acf035142ca2021c693f8b050dc231f1edaeb8e1
avaron <avaron@ED1939.edicom.es>
2019-07-10 17:29:10 +0200

    [GREEN] return true when is more than three

--- GameOfLife/src/main/java/com/edicom/GameOfLife/GameOfLife.java
+++ GameOfLife/src/main/java/com/edicom/GameOfLife/GameOfLife.java
@@ -1,25 +1,29 @@
 package com.edicom.GameOfLife;
 
 public class GameOfLife {
-	
-	
+
 	public static boolean isAlive(boolean cell) {
-		
+
 		return cell;
-		
-	}
-	
-	public static boolean isLessThanTwo(int neighbors) {
-		
-		return neighbors < 2;
-		
-	}
-	
-	public static boolean isTwoOrThree(int neighbors) {
-		
-		return neighbors == 2 || neighbors == 3;
-		
+
 	}
 
+	public static boolean isLessThanTwo(int neighbors) {
+
+		return neighbors < 2;
+
+	}
+
+	public static boolean isTwoOrThree(int neighbors) {
+
+		return neighbors == 2 || neighbors == 3;
+
+	}
+
+	public static boolean isMoreThanThree(int neighbors) {
+
+		return neighbors > 3;
+
+	}
 
 }
