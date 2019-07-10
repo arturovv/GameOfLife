9f7ecbe5542d7d774a6b1b8f67fcd6f0b58a9637
avaron <avaron@ED1939.edicom.es>
2019-07-10 17:37:38 +0200

    [red] created nextStep

--- GameOfLife/src/test/java/com/edicom/GameOfLife/GameOfLifeTest.java
+++ GameOfLife/src/test/java/com/edicom/GameOfLife/GameOfLifeTest.java
@@ -72,6 +72,31 @@ public class GameOfLifeTest {
 			{4, false},
 
 			};
+	}	
+	
+	@DataProvider
+	public static Object[][] dataCellProviderForNextStep(){
+		
+		return new Object[][] {
+			{true, 0 , false},
+			{true, 1 , false},
+			{true, 2 , true},
+			{true, 3 , true},
+			{true, 4 , false},
+			{true, 5 , false},
+			{true, 6 , false},
+			{true, 7 , false},
+			{true, 8 , false},
+			{false, 0 , false},
+			{false, 1 , false},
+			{false, 2 , false},
+			{false, 3 , true},
+			{false, 4 , false},
+			{false, 5 , false},
+			{false, 6 , false},
+			{false, 7 , false},
+			{false, 8 , false},
+			};
 	}
 	
 	@Test
@@ -125,6 +150,19 @@ public class GameOfLifeTest {
 		Assert.assertEquals(expected, result);
 		
 	}
+	
+	@Test
+	@UseDataProvider("dataCellProviderForNextStep")
+	public void testNextStep(boolean cell, int neighbors, boolean expected) {
+		
+		boolean result = GameOfLife.nextStep(cell, neighbors);
+		
+		Assert.assertEquals(expected, result);
+		
+	}
+	
+	
+	
 	
 	
 
