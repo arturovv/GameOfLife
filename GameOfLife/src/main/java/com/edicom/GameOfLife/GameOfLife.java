5e047d71dd1bb80b968a1b54072be52960c21edc
avaron <avaron@ED1939.edicom.es>
2019-07-10 17:07:28 +0200

    [GREEN] Return True/False when alive/died in isAlive method with DataProvider

---
+++ GameOfLife/src/main/java/com/edicom/GameOfLife/GameOfLife.java
@@ -0,0 +1,11 @@
+package com.edicom.GameOfLife;
+
+public class GameOfLife {
+	
+	public static boolean isAlive(boolean cell) {
+		
+		return cell;
+		
+	}
+
+}
--- GameOfLife/src/test/java/com/edicom/GameOfLife/GameOfLifeTest.java
+++ GameOfLife/src/test/java/com/edicom/GameOfLife/GameOfLifeTest.java
@@ -2,11 +2,32 @@ package com.edicom.GameOfLife;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+
+
+@RunWith(DataProviderRunner.class)
 public class GameOfLifeTest {
 	
+	@DataProvider
+	public static Object[][] dataCellProviderForAlive(){
+		
+		return new Object[][] {
+			{true, true},
+			{false, false},
+			{true, true},
+			};
+	}
+	
+	
+	
 	@Test
+	@UseDataProvider("dataCellProviderForAlive")
 	public void itShouldBeTrueIfAlive(boolean cell, boolean expected) {
+		
 		
 		
 		boolean result = GameOfLife.isAlive(cell);
